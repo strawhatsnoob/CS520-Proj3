b7a29e3081e0d85c983f2d686cd3772a28bc169c
Mukunthan Sriram Balaji <muku0210@gmail.com>
2023-12-10 20:08:25 -0500

    modified APEX_run stage

--- apex_cpu_pipeline_simulator/apex_cpu.c
+++ apex_cpu_pipeline_simulator/apex_cpu.c
@@ -2406,18 +2406,18 @@ APEX_cpu_run(APEX_CPU *cpu)
             printf("APEX_CPU: Simulation Complete, cycles = %d instructions = %d\n", cpu->clock, cpu->insn_completed);
             break;
         }
-        APEX_memory(cpu);
-        APEX_execute(cpu);
-        APEX_ROB(cpu);
+        APEX_fetch(cpu);
+        APEX_decode(cpu);
+        APEX_dispatch(cpu);
         APEX_IntFu(cpu);
-        APEX_MAU(cpu);
         APEX_MulFu(cpu);
         APEX_BFU(cpu);
         APEX_AFU(cpu);
-        APEX_dispatch(cpu);
         APEX_LSQ(cpu);
-        APEX_decode(cpu);
-        APEX_fetch(cpu);
+        APEX_MAU(cpu);
+        APEX_ROB(cpu);
+        APEX_memory(cpu);
+        APEX_execute(cpu);
 
         print_reg_file(cpu);
         printf("P %d \n", cpu->positive_flag);
